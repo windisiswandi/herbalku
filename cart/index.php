71ac76ab2cb0ff0d28b539da8db7e4ed3f4a4b3a
windisiswandi <windisiswandi89@gmail.com>
2024-09-05 23:24:36 +0800

    feat: type

--- cart/index.php
+++ cart/index.php
@@ -28,7 +28,7 @@ $page = 'KERANJANG';
             $cek_cart = mysqli_num_rows($select_cart);
         ?>
             <div class="header_cart" id="header_cart">
-                <p>Total Kerajnang Belanja <span><?php echo $cek_cart; ?></span></p>
+                <p>Total Keranjang Belanja <span><?php echo $cek_cart; ?></span></p>
             </div>
             <div class="box_isi_cart">
                 <?php
